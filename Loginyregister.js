a76ef47747ff1ec3c717ec5eb5c2baf59803e3ff
Alejandro Diaz santos <94212721+Alejandro-DS@users.noreply.github.com>
2023-06-26 19:20:47 -0600

    mi primer commit
    Adicción de Archivos de índex estilos Js y bd

---
+++ Loginyregister.js
@@ -0,0 +1,51 @@
+//Declaracion de variables
+
+var Formulario_Login = document.querySelector(".Formulario_Login")
+
+var Formulario_Registrarse = document.querySelector(".Formulario_Registrarse")
+
+
+function Registrarse(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
